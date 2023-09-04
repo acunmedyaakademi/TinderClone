075bba8a33662d983442a4f50397dbe4d5cebb52
cihankusmus <kusmusc@gmail.com>
2023-09-04 05:25:49 +0300

    data pulled from database

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 const tinderContainer = document.querySelector('.tinder');
 const allCards = document.querySelectorAll('.tinderCard');
 const nope = document.querySelector('#nope');
@@ -28,8 +27,8 @@ allCards.forEach(function (el) {
         tinderContainer.classList.toggle('tinder_love', event.deltaX > 0);
         tinderContainer.classList.toggle('tinder_nope', event.deltaX < 0);
 
-        let xMulti = event.deltaX * 0.3;
-        let yMulti = event.deltaY / 80;
+        let xMulti = event.deltaX * 0.2;
+        let yMulti = event.deltaY / 300;
         let rotate = xMulti * yMulti;
 
         el.style.transform = 'translate(' + event.deltaX + 'px, ' + event.deltaY + 'px) rotate(' + rotate + 'deg)';
@@ -91,3 +90,57 @@ let loveListener = createButtonListener(true);
 
 nope.addEventListener('click', nopeListener);
 love.addEventListener('click', loveListener);
+
+
+
+const requestUsersUrl = 'https://mavnvirtkvhbnvtmiddk.supabase.co/rest/v1/tinder';
+let users = [];
+const authorizationKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hdm52aXJ0a3ZoYm52dG1pZGRrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MzUwMDYyOSwiZXhwIjoyMDA5MDc2NjI5fQ.cWFtEEnx4NWVVH3gmSDbHZ4hgi5ZJ4mZmKQkChz-e9o';
+const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hdm52aXJ0a3ZoYm52dG1pZGRrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MzUwMDYyOSwiZXhwIjoyMDA5MDc2NjI5fQ.cWFtEEnx4NWVVH3gmSDbHZ4hgi5ZJ4mZmKQkChz-e9o';
+
+function loadData() {
+    return fetch('https://mavnvirtkvhbnvtmiddk.supabase.co/rest/v1/tinder?id=eq.2',{
+      method: 'GET',
+      headers: {
+          'Authorization': `Bearer ${authorizationKey}`, 
+          'ApiKey': apiKey, 
+        },
+    })
+        .then(response => {
+            
+            return response.json();
+        });
+}
+
+async function render() {
+    try {
+        users = await loadData();
+        
+        const userInfo = document.querySelector('.userInfos');
+        userInfo.innerHTML = '';
+
+        for (let i = 0; i < 1000 && i < users.length; i++) {
+            const currPost = users[i];
+
+            userInfo.innerHTML += `
+                <div class="userInfo">
+                    <h3>${currPost.name}</h3>
+                    
+                    
+                </div>
+            `;
+        }
+
+       
+    } catch (error) {
+        console.error('Hata: ', error);
+        showLoadError();
+    }
+}
+
+
+function showLoadError() {
+    alert('Veri çekme işlemi başarısız oldu: 404');
+}
+
+render();
