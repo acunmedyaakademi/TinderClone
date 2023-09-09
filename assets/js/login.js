ab19ec099dd61d9844cab8d1535397fc0cccee01
Mustafa YÜKSEL <mustafayksl1407@gmail.com>
2023-09-09 19:55:25 +0300

    logIn işlemi başarılı

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,34 +1,40 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+// import { createClient } from "@supabase/supabase-js";
 
-const SupabaseUrl = 'https://mavnvirtkvhbnvtmiddk.supabase.co';
-const SecretKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hdm52aXJ0a3ZoYm52dG1pZGRrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MzUwMDYyOSwiZXhwIjoyMDA5MDc2NjI5fQ.cWFtEEnx4NWVVH3gmSDbHZ4hgi5ZJ4mZmKQkChz-e9o';
+const SupabaseUrl = 'https://mavnvirtkvhbnvtmiddk.supabase.co'
+const SecretKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hdm52aXJ0a3ZoYm52dG1pZGRrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MzUwMDYyOSwiZXhwIjoyMDA5MDc2NjI5fQ.cWFtEEnx4NWVVH3gmSDbHZ4hgi5ZJ4mZmKQkChz-e9o'
 
-const supaBase = createClient(SupabaseUrl, SecretKey);
+const supaBase = createClient(SupabaseUrl, SecretKey)
 
-const PostForm =document.querySelector('.post-form');
-const PostList =document.querySelector('.post-list');
+const PostForm = document.querySelector('.post-form')
+const PostList = document.querySelector('.post-list')
 
 // const loginForm = document.getElementById('loginForm');
 
-PostForm.addEventListener('submit', async (event) => {
-  event.preventDefault(); 
+PostForm.addEventListener('submit', async event => {
+  event.preventDefault()
 
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
+  const username = document.getElementById('username').value
+  const password = document.getElementById('password').value
 
   try {
-    const { user, error } = await supaBase.auth.signIn({
-      email: username, 
-      password: password,
-    });
+    const {
+      data: { user },
+      error
+    } = await supaBase.auth.signInWithPassword({
+      username: username,
+      password: password
+    })
 
-    if (error) {
-      console.error('Giriş hatası:', error.message);
-      
+    console.log(user, error)
+    if (user) {
+      console.log('Giriş başarılı:', user)
     } else {
-      console.log('Giriş başarılı:', user);
+      window.location.replace('index.html')
+      console.error('Giriş hatası:', error.message)
     }
   } catch (error) {
-    // console.error('Bir hata oluştu:', error);
-  }
-});
+    console.error('Bir hata oluştu:', error)
+  }
+})
