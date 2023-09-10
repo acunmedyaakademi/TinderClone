4a59ac8a497487b2b46cb66299d62980f3c51a71
Mustafa YÜKSEL <mustafayksl1407@gmail.com>
2023-09-10 23:18:30 +0300

    register and login is success

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,39 +1,43 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-// import { createClient } from "@supabase/supabase-js";
+// import { createClient } from "../../node_modules/@supabase/supabase-js/dist/umd/supabase";
 
-const SupabaseUrl = 'https://mavnvirtkvhbnvtmiddk.supabase.co'
-const SecretKey =
+const supabaseUrl = 'https://mavnvirtkvhbnvtmiddk.supabase.co'
+const secretKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hdm52aXJ0a3ZoYm52dG1pZGRrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MzUwMDYyOSwiZXhwIjoyMDA5MDc2NjI5fQ.cWFtEEnx4NWVVH3gmSDbHZ4hgi5ZJ4mZmKQkChz-e9o'
 
-const supaBase = createClient(SupabaseUrl, SecretKey)
+const supabase = createClient(supabaseUrl, secretKey)
 
-const PostForm = document.querySelector('.post-form')
-const PostList = document.querySelector('.post-list')
+const postForm = document.querySelector('.post-form')
+const postList = document.querySelector('.post-list')
 
 // const loginForm = document.getElementById('loginForm');
 
-PostForm.addEventListener('submit', async event => {
+postForm.addEventListener('submit', async event => {
   event.preventDefault()
 
-  const username = document.getElementById('username').value
-  const password = document.getElementById('password').value
+  const username = document.querySelector('#username')
+  const password = document.querySelector('#password')
 
   try {
-    const {
-      data: { user },
-      error
-    } = await supaBase.auth.signInWithPassword({
-      username: username,
-      password: password
-    })
+    // const { data, error } = await supabase.auth.signInWithPassword({
+    //   email: email,
+    //   password: password
+    // })
+    // console.log(error, data);
+    const {data: {user}, error}
+     = await supabase.auth.signInWithPassword({
+      email: username.value,
+      password: password.value
+    }).then((data) => data)
 
-    console.log(user, error)
-    if (user) {
-      console.log('Giriş başarılı:', user)
-    } else {
-      window.location.replace('index.html')
-      console.error('Giriş hatası:', error.message)
-    }
+     if (user) {
+      console.log('Giriş başarılı:')
+      console.log(user);
+      window.location = '/'
+     } else {
+       
+       console.error('Giriş hatası:', error)
+     }
   } catch (error) {
     console.error('Bir hata oluştu:', error)
   }
--- assets/js/register.js
+++ assets/js/register.js
@@ -1,4 +1,6 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import {
+    createClient
+} from 'https://esm.sh/@supabase/supabase-js@2';
 
 const supabaseUrl = 'https://mavnvirtkvhbnvtmiddk.supabase.co';
 const secretKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hdm52aXJ0a3ZoYm52dG1pZGRrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MzUwMDYyOSwiZXhwIjoyMDA5MDc2NjI5fQ.cWFtEEnx4NWVVH3gmSDbHZ4hgi5ZJ4mZmKQkChz-e9o';
@@ -12,31 +14,51 @@ const addPost = async (e) => {
     e.preventDefault();
     const postFormData = Object.fromEntries(new FormData(postForm));
     console.log(postFormData);
-    await fetch(`${supabaseUrl}/rest/v1/users`, {
-        method: 'POST',
-        body: JSON.stringify({
-            username: postFormData.username,
-            password: postFormData.password,
-            name: postFormData.name,
-            age: postFormData.age,
-            image: postFormData.image.name
-        }),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${secretKey}`,
-            'apikey': secretKey
-        }
-    })
+    
 
-    await supabase
-    .storage
-    .from('image')
-    .upload(postFormData.image.name, postFormData.image, {
-        cacheControl: '3600',
-        upsert: false
-    });
+    const data = await supabase.auth.signUp({   email: postFormData.email,   password: postFormData.password, })
+    if (data) {
+        await fetch(`${supabaseUrl}/rest/v1/users`, {
+            method: 'POST',
+            body: JSON.stringify({
+                email: postFormData.email,
+                password: postFormData.password,
+                name: postFormData.name,
+                age: postFormData.age,
+                image: postFormData.image.name
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${secretKey}`,
+                'apikey': secretKey
+            }
+        })
+    }
+    // console.log(postFormData);
+    // const {
+    //     data,
+    //     error
+    // } = await supabase.auth.signUp({
+    //     email: postFormData.email,
+    //     password: postFormData.password,
+    //     options: {
+    //         data: {
+    //             name: 'asdfafs',
+    //             email: 'sajda@gmail.com',
+    //             age: 22
+    //         }
+    //     }
+    // })
     
-    
+    //  await supabase
+    //      .storage
+    //      .from('image')
+    //      .upload(postFormData.image.name, postFormData.image, {
+    //          cacheControl: '3600',
+    //          upsert: false
+    //      });
+
+
 }
 
 postForm.addEventListener('submit', addPost);
